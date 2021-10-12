f3493f1bcac5c24606a60d637bd3f1e29609c7ff
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-10-12 23:42:21 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const redux = require('redux');
+const createStore = redux.createStore();
+
+// Action name---
+const BUY_CAKE = 'BUY_CAKE';
+
+// Action--
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action',
+  };
+}
+
+// Reducer--
+// (previousState, action) => newState
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+// kalau ada perubahan state, jalankan function tersebut.
+store.subscribe(() => console.log('Update state', store.getState()));
