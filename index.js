3f61f6a53d5cf7548a503b2f3e5d0f0b82fbad62
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-10-13 10:36:43 +0700

    add multiple reducers (combineReducers)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const redux = require('redux');
+const combineReducers = redux.combineReducers;
 const createStore = redux.createStore;
 
 // Action name---
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 // Action--
 function buyCake() {
@@ -12,13 +14,24 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: 'Second redux action',
+  };
+}
+
 // Reducer--
 // (previousState, action) => newState
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numberOfIceCream: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -31,8 +44,24 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-console.log(reducer);
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCream: state.numberOfIceCream - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
 console.log('Initial state', store.getState());
 
 // kalau ada perubahan state, jalankan function tersebut.
@@ -43,4 +72,7 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
